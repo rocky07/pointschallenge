f60820296e3305aa7d7c74def75016d26e65bda1
rakeshramesan <rakesh.ramesan@tmhighland.com>
2023-09-08 17:16:11 -0500

    initial commit

---
+++ src/main/java/com/demo/pointschallenge/pointschallenge/PointschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pointschallenge.pointschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointschallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/pointschallenge/pointschallenge/PointschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pointschallenge.pointschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointschallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
